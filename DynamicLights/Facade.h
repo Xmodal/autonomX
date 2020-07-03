05e1364e9efc41c54470e3e900d7f79374989062
Simon Demeule <simondemeule@gmail.com>
2020-07-03 13:51:07 -0400

    Added documentation to Facade

--- DynamicLights/Facade.h
+++ DynamicLights/Facade.h
@@ -18,6 +18,18 @@
 #include <QObject>
 #include <QQmlPropertyMap>
 
+// the Facade objects requires that the object it aliases emits a valueChanged(const QString &key, const QVariant &value) signal when any of its QProperties is changed.
+// once created, the Facade will have a copy of the QObject's properties, but they are not linked yet. to link them, use:
+
+// connect generator changes to facade
+// QObject::connect(alias, &Generator::valueChanged, fadcade, &Facade::updateValueRelay);
+
+// connect facade changes to generator
+// QObject::connect(facade, &Facade::valueChanged, alias, &Generator::updateValue);
+
+// this unfortunately can't happen in Facade's constructor because QObject doesn't have a valueChanged() signal by default
+
+
 class Facade : public QQmlPropertyMap
 {
     Q_OBJECT

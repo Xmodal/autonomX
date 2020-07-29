de5602dbdb1eb565254d353e6eaf231ad8724f68
Simon Demeule <simondemeule@gmail.com>
2020-07-28 22:49:03 -0400

    Added algorithm for finding unique IDs, removed hash maps, added comments for next steps in AppModel implementation

--- autonomx/AppModel.cpp
+++ autonomx/AppModel.cpp
@@ -14,22 +14,40 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "AppModel.h"
+#include "SpikingNet.h"
 
 AppModel::AppModel() {
-    // init data (list form)
-    generatorsList = QSharedPointer<QList<QSharedPointer<Generator>>>(new QList<QSharedPointer<Generator>>());
-    generatorFacadesList = QSharedPointer<QList<QSharedPointer<GeneratorFacade>>>(new QList<QSharedPointer<GeneratorFacade>>());
-
-    // init data (hashmap form)
-    generatorsHash = QSharedPointer<QHash<int, QSharedPointer<Generator>>>(new QHash<int, QSharedPointer<Generator>>());
-    generatorFacadesHash = QSharedPointer<QHash<int, QSharedPointer<GeneratorFacade>>>(new QHash<int, QSharedPointer<GeneratorFacade>>());
+    // init data (lists)
+    generators = QSharedPointer<QList<QSharedPointer<Generator>>>(new QList<QSharedPointer<Generator>>());
+    generatorFacades = QSharedPointer<QList<QSharedPointer<GeneratorFacade>>>(new QList<QSharedPointer<GeneratorFacade>>());
 
     // init data (unique elements)
-    QSharedPointer<GeneratorModel> generatorModel = QSharedPointer<GeneratorModel>(new GeneratorModel(generatorFacadesList));
+    QSharedPointer<GeneratorModel> generatorModel = QSharedPointer<GeneratorModel>(new GeneratorModel(generatorFacades));
+
+    // init engines
+    computeEngine = QSharedPointer<ComputeEngine>(new ComputeEngine(generators));
+    oscEngine = QSharedPointer<OscEngine>(new OscEngine());
+
+    // init threads
+    computeThread = QSharedPointer<QThread>(new QThread());
+    oscThread = QSharedPointer<QThread>(new QThread());
 }
 
-void AppModel::startThreads() {
+void AppModel::start() {
+    computeThread->start(QThread::TimeCriticalPriority);
+    computeEngine->moveToThread(computeThread.data());
 
+    // we need to move all Generator instances to computeThread
+    for(QList<QSharedPointer<Generator>>::iterator it = generators.get()->begin(); it != generators.get()->end(); it++) {
+        (*it)->moveToThread(computeThread.data());
+    }
+
+    oscThread->start(QThread::TimeCriticalPriority);
+    oscEngine->moveToThread(oscThread.data());
+
+    computeEngine->start();
+
+    started = true;
 }
 
 QThread* AppModel::getComputeThread() const {
@@ -41,33 +59,81 @@ QThread* AppModel::getOscThread() const {
 }
 
 void AppModel::createGenerator() {
+    // find a free ID for the new generator
+    // create a temporary list of taken IDs
+    // we do this instead of using generatorsList because we will want to sort it, and this is thread-unsafe since computeEngine might be iterating generators it at any time
+    QList<int> takenIDs;
+    for(QList<QSharedPointer<Generator>>::iterator it = generators->begin(); it != generators->end(); it++) {
+        int takenID = (*it)->getID();
+        takenIDs.append(takenID);
+    }
+    // we sort the taken IDs
+    std::sort(takenIDs.begin(), takenIDs.end());
+    // we search for a free ID by looking through the sorted taken IDs, incrementing the next ID if it is already taken
+    int nextID = 0;
+    for(QList<int>::iterator it = takenIDs.begin(); it != takenIDs.end(); it++) {
+        int takenID = (*it);
+        if(nextID == takenID) {
+            nextID++;
+        }
+    }
+
+    // create a new generator
+    QSharedPointer<Generator> generator = QSharedPointer<Generator>(new SpikingNet(nextID));
+
+    // move the Generator to computeThread if it is started
+    if(started) {
+        generator->moveToThread(computeThread.data());
+    }
+
+    // we can't add to the Generator list directly because this could break things on the computeThread.
+    // add it later on the computeThread thread once it is safe to do so
+
+    // we can only create an OscSender / OscReceiver pair once the Generator exists because we have to setup some connections.
+    // this means we have to do this after the pervious operation is done.
+
+    // create a GeneratorFacade and add it to the list
+    QSharedPointer<GeneratorFacade> generatorFacade = QSharedPointer<GeneratorFacade>(new GeneratorFacade(generator.data()));
+    generatorFacades->push_back(generatorFacade);
+    // once the list is changed, update the GeneratorModel connections
     generatorModel->relinkAliasConnections();
 }
 
 void AppModel::deleteGenerator(int id) {
+    // we can't delete on the Generator list directly because this could break things on the computeThread.
+    // delete it later on the computeThread thread once it is safe to do so
+
+    // we can delete an OscSender / OscReceiver pair anytime as long as we update OscEngine to not kill itself if an invalid generator id is used (which would happen if the matching OscSender / OscReceiver were deleted before the Generator was deleted, leaving enough time for a ComputeEngine cycle to finish)
+    // it's actually much simpler to just handle in ComputeEngine, in the slot dedicated to Generator deletion, much like how we will manage OscSender / OscReceiver creation.
+
+    // once the list is changed, update the GeneratorModel connections
     generatorModel->relinkAliasConnections();
 }
 
 bool AppModel::validateNewGeneratorName(QString name) {
     bool valid = true;
-    for(QList<QSharedPointer<Generator>>::iterator it = generatorsList.get()->begin(); it != generatorsList.get()->end() && valid; it++) {
+    for(QList<QSharedPointer<Generator>>::iterator it = generators->begin(); it != generators->end() && valid; it++) {
         valid = name != it->data()->getName();
     }
     return valid;
 }
 
 Generator* AppModel::getGenerator(int id) const {
-    if(!generatorsHash->contains(id)) {
-        throw std::runtime_error("generator does not exist");
-    }
-    return generatorsHash->value(id).data();
+    for(QList<QSharedPointer<Generator>>::iterator it = generators->begin(); it != generators->end(); it++) {
+        if(id == (*it)->getID()) {
+            return (*it).data();
+        }
+     }
+    return nullptr;
 }
 
 GeneratorFacade* AppModel::getGeneratorFacade(int id) const {
-    if(!generatorFacadesHash->contains(id)) {
-        throw std::runtime_error("generator facade does not exist");
-    }
-    return generatorFacadesHash->value(id).data();
+    for(QList<QSharedPointer<GeneratorFacade>>::iterator it = generatorFacades->begin(); it != generatorFacades->end(); it++) {
+        if(id == (*it)->value("id").toInt()) {
+            return (*it).data();
+        }
+     }
+    return nullptr;
 }
 
 GeneratorModel* AppModel::getGeneratorModel() const {

a37877cf4997d064482a06752970289e8eedb64d
Simon Demeule <simondemeule@gmail.com>
2020-08-03 21:35:05 -0400

    Fix nearest-neightbord filter not working

--- autonomx/GeneratorLatticeRenderer.cpp
+++ autonomx/GeneratorLatticeRenderer.cpp
@@ -109,7 +109,7 @@ void GeneratorLatticeRenderer::render() {
         glBindTexture(GL_TEXTURE_2D, texture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, *allocatedWidth, *allocatedHeight, 0, GL_RED, GL_FLOAT, *latticeData);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         if(flagSuper) {
             // bind supersampling framebuffer

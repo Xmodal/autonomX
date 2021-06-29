69a3356d337a8b6a070d3ca42f8c581f3e58c963
Julien Vermette <julien.vermette@outlook.com>
2021-06-28 22:05:49 -0400

    clean up code & revert initial gen index in QML

--- autonomx/GeneratorFacade.cpp
+++ autonomx/GeneratorFacade.cpp
@@ -61,10 +61,6 @@ GeneratorFacade::GeneratorFacade(Generator *generator) : QQmlPropertyMap(this, n
     // connect facade changes to generator
     QObject::connect(this, &GeneratorFacade::valueChanged, generator, &Generator::updateValue, Qt::QueuedConnection);
 
-    // ba ba booey
-//    QObject::connect(this, &GeneratorFacade::initializeFired, generator, &Generator::initialize, Qt::QueuedConnection);
-//    QObject::connect(this, &GeneratorFacade::resetRegionsFired, generator, &Generator::resetRegions, Qt::QueuedConnection);
-
     // [DEPRECATED] connect facade region model changes to region set
     // NO LONGER NECESSARY! :) but i'll keep it here for reference in case things go to shit somehow
 //    QObject::connect(inputRegionModel.data(), &GeneratorRegionModel::addRegionFromModel, generator->getInputRegionSet(), &GeneratorRegionSet::addRegion, Qt::QueuedConnection);
